bce4e632b40968ba260b12e81629f406dc70d1a1
64J0 <vinigaio97@gmail.com>
2024-01-21 12:02:10 -0300

    chore: remove Ping handler

--- Handlers/Ping.fs
+++
@@ -1,9 +0,0 @@
-namespace Handlers.Ping
-
-open Microsoft.AspNetCore.Http
-open Giraffe
-
-module GET =
-
-    let ping: HttpHandler =
-        fun (next: HttpFunc) (ctx: HttpContext) -> text "pong" next ctx
