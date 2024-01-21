4463d0ac8d1f1f44fe9e5d0905bb90b3b5e142da
64J0 <vinigaio97@gmail.com>
2024-01-21 12:02:30 -0300

    feat: add summary to ExtractRouteInfo middleware

--- Middlewares/ExtractRouteInfo.fs
+++ Middlewares/ExtractRouteInfo.fs
@@ -3,12 +3,15 @@ module Middlewares
 open Microsoft.AspNetCore.Http
 open Giraffe
 
+/// Extract the dynamic route information and add to the
+/// HTTP context object
 let extractRouteInfo (args: 'T) : HttpHandler =
     fun (next: HttpFunc) (ctx: HttpContext) ->
         ctx.Items.Add("routeInfo", args)
         next ctx
 
-// TODO extract to another location
+/// Helper function that extracts the dynamic route information
+/// from the HTTP context object
 let getRouteInfo<'T> (ctx: HttpContext) : Result<'T, string> =
     let routeInfo = ctx.Items.TryGetValue "routeInfo"
 
